0cc9ddffa6910582f5199de29198116acbb8dff3
sbuntrock <seth@buntrock.net>
2019-02-28 14:14:26 -0600

    Added simple build/install script

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+make -C st
+make -C st install
+make -C slock
+make -C slock install
+make -C slstatus
+make -C slstatus install
+make -C dmenu
+make -C dmenu install
+make -C dwm
+make -C dwm install
